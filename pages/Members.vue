34ec24e02367c445c206aded8beb76c947d62c70
lucasvarino <lucasvarino@gmail.com>
2025-03-03 01:43:28 -0300

    Update Members page layout for responsive design and improved spacing

--- pages/Members.vue
+++ pages/Members.vue
@@ -1,8 +1,8 @@
 
 <template>
-    <div class="h-screen flex items-center">
-        <div class="container mx-auto">
-            <div class="grid grid-cols-8 gap-4">
+    <div class="min-h-screen flex items-center">
+        <div class="container mx-auto px-4">
+            <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-4">
                 <div v-for="member in members" :key="member.id" class="flex flex-col items-center gap-2">
                     <MemberIcon :member-id="member.number" />
                     <p>{{ member.name }}</p>
