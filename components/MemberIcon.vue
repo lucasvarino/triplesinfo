b916e20ebeaedd37210c485dc3979a36680669ce
lucasvarino <lucasvarino@gmail.com>
2025-03-03 01:42:05 -0300

    Adding hover in memberIcon

--- components/MemberIcon.vue
+++ components/MemberIcon.vue
@@ -1,6 +1,10 @@
 <template>
-    <div class="p-4 border border-gray-700 rounded-full w-fit">
+    <div class="group relative p-4 border border-gray-700 rounded-full w-fit">
         <nuxt-img :src="`/images/members/icons/s${memberId}.png`" alt="" class="w-24 h-24 rounded-full object-cover" />
+        <span
+            class="absolute inset-0 flex items-center justify-center text-3xl font-bold opacity-0 group-hover:opacity-100 transition-opacity duration-300 outline-text">
+            S{{ memberId }}
+        </span>
     </div>
 </template>
 
@@ -11,6 +15,13 @@ const props = defineProps({
     memberId: {
         type: Number,
         required: true
-    },
+    }
 })
 </script>
+
+<style scoped>
+.outline-text {
+    color: transparent;
+    -webkit-text-stroke: 1px black;
+}
+</style>
