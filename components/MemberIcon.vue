ce330879a45d00a0334c15b5e80a16f3d3c12aea
lucasvarino <lucasvarino@gmail.com>
2025-03-03 01:36:12 -0300

    Add MemberIcon component and Members page layout

---
+++ components/MemberIcon.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="p-4 border border-gray-700 rounded-full w-fit">
+        <nuxt-img :src="`/images/members/icons/s${memberId}.png`" alt="" class="w-24 h-24 rounded-full object-cover" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps({
+    memberId: {
+        type: Number,
+        required: true
+    },
+})
+</script>
---
+++ pages/Members.vue
@@ -0,0 +1,19 @@
+
+<template>
+    <div class="h-screen flex items-center">
+        <div class="container mx-auto">
+            <div class="grid grid-cols-8 gap-4">
+                <div v-for="member in members" :key="member.id" class="flex flex-col items-center gap-2">
+                    <MemberIcon :member-id="member.number" />
+                    <p>{{ member.name }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import MemberIcon from '~/components/MemberIcon.vue';
+import { members } from '~/data/members';
+
+</script>
